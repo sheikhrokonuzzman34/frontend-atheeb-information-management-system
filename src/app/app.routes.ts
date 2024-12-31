f535d828313eecde87295e4554295098912d6716
S. M. Rafi <smrafialam@gmail.com>
2024-12-31 17:53:03 +0600

    completed registration page design

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,13 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { RegistrationComponent } from './registration/registration.component';
 
 export const routes: Routes = [
     { path: 'login', component: LoginComponent },
+    { path: 'registration', component: RegistrationComponent },
     { path: 'dashboard', component: DashboardComponent },
       // Add other routes here
-    // { path: '', redirectTo: '/login', pathMatch: 'full' }, // Redirect to login by default
+    { path: '', redirectTo: '/dashboard', pathMatch: 'full' }, // Redirect to login by default
     // { path: '**', redirectTo: '/login' } // Wildcard route for a 404 page
 ];
